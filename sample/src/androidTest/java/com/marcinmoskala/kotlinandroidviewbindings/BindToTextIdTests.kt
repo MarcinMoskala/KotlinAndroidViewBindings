83d62c7bf0eab55e4138870c8855f69bf60701ab
Marcin Moskala <marcinmoskala@gmail.com>
2017-05-08 17:14:49 +0200

    textViewBindIdToText test

--- sample/src/androidTest/java/com/marcinmoskala/kotlinandroidviewbindings/BindToTextIdTests.kt
+++ sample/src/androidTest/java/com/marcinmoskala/kotlinandroidviewbindings/BindToTextIdTests.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import android.widget.Button
+import android.widget.TextView
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,8 +27,23 @@ class BindToTextIdTests {
         assertEquals(text1id, buttonTextId)
         assertEquals(text1, button.text.toString())
         buttonTextId = text2id
-        assertEquals(text2id
-                , buttonTextId)
+        assertEquals(text2id, buttonTextId)
         assertEquals(text2, button.text.toString())
+    }
+
+    @Test
+    fun textViewBindIdToText() {
+        val textView = TextView(context)
+        var buttonTextId by textView.bindToTextId()
+        val text1id = android.R.string.copy
+        val text1 = context.getText(android.R.string.copy)
+        val text2id = android.R.string.cancel
+        val text2 = context.getText(android.R.string.cancel)
+        buttonTextId = text1id
+        assertEquals(text1id, buttonTextId)
+        assertEquals(text1, textView.text.toString())
+        buttonTextId = text2id
+        assertEquals(text2id, buttonTextId)
+        assertEquals(text2, textView.text.toString())
     }
 }
