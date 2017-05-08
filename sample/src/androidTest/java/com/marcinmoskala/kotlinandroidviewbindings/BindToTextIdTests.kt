70e0ef965da62b6e6830a5d95d3004e5370a4e58
Marcin Moskala <marcinmoskala@gmail.com>
2017-05-08 17:18:15 +0200

    editTextBindIdToText test

--- sample/src/androidTest/java/com/marcinmoskala/kotlinandroidviewbindings/BindToTextIdTests.kt
+++ sample/src/androidTest/java/com/marcinmoskala/kotlinandroidviewbindings/BindToTextIdTests.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -34,6 +35,22 @@ class BindToTextIdTests {
     @Test
     fun textViewBindIdToText() {
         val textView = TextView(context)
+        var buttonTextId by textView.bindToTextId()
+        val text1id = android.R.string.copy
+        val text1 = context.getText(android.R.string.copy)
+        val text2id = android.R.string.cancel
+        val text2 = context.getText(android.R.string.cancel)
+        buttonTextId = text1id
+        assertEquals(text1id, buttonTextId)
+        assertEquals(text1, textView.text.toString())
+        buttonTextId = text2id
+        assertEquals(text2id, buttonTextId)
+        assertEquals(text2, textView.text.toString())
+    }
+
+    @Test
+    fun editTextBindIdToText() {
+        val textView = EditText(context)
         var buttonTextId by textView.bindToTextId()
         val text1id = android.R.string.copy
         val text1 = context.getText(android.R.string.copy)
